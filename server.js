776fccbb5b51cf9a777b1f641a21161b2b909110
jennj <jennjenn93@gmail.com>
2022-04-17 00:00:25 -0500

    adding notes GET

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const uuid = require('uuid');
-
+const notes = require('./Develop/db/db.json')
 const app = express();
 
+const PORT = process.env.PORT || 3001;
 
-
-
+app.get('/api/notes', (req,res) => {
+    res.send('It worked!');
+});
 
 
 app.listen(3001, () => {
