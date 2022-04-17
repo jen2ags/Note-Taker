e9e212ca4ad475d4a62d89572c949e88861cec86
jennj <jennjenn93@gmail.com>
2022-04-16 21:51:31 -0500

    requires and port

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const uuid = require('uuid');
+
+const app = express();
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server is now on PORT 3001!`)
+});
