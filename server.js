bfd977aa55b4e2206829c6a915f0a702fa3178cc
jennj <jennjenn93@gmail.com>
2022-04-21 17:20:00 -0500

    adding notes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const fs = require('fs');
 const uuid = require('uuid');
-const notes = require('./Develop/db/db.json')
+const notes = require('./Develop/db/db.json');
 const app = express();
 const path = require('path');
+const { notes } = require('./Develop/db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
