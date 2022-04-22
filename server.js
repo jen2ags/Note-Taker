b28502e89a3750a8602d0b8c82c723c16c4d2503
jennj <jennjenn93@gmail.com>
2022-04-21 20:47:33 -0500

    pushing notes into newnote and write to file

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.post('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNotes = req.body;
     newNotes.id = uuid.v4();
-    notes.toString();
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
 
     res.json(notes);
 });
